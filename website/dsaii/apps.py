b554dfcabd41b78ebd5f672a903b0a183be36dd4
khushi <khushi.sali2002@gmail.com>
2021-10-24 15:12:53 +0530

    push

---
+++ website/dsaii/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DsaiiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dsaii'
---
+++ website/dsaii/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+#from .models import MenuItem, Category, OrderModel
+from django.core.mail import send_mail
+from django.conf import settings
+import datetime
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
