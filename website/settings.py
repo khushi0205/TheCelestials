a69a3d4c5aa0a3ad174bc1b19606b04aad3479f9
khushi <khushi.sali2002@gmail.com>
2021-11-08 00:01:46 +0530

    align

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jq3oj#3ps9jq_l0-e56$jj=gi(dux9gv3&zc5zjgaq&rsb)b7b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','dit-dsaii.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0','dit-dsaii.herokuapp.com']
 
 
 # Application definition
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tirals.trials@gmail.com'
-EMAIL_HOST_PASSWORD = 'Trials321'
+EMAIL_HOST_PASSWORD = ''
